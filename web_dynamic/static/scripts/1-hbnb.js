0e5987ac49a3e62b0dcdab1675e9f8e16a78e464
altoids <noelreeds01@gmail.com>
2025-05-14 09:21:58 +0300

    adds amenities filters to search

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,15 +1,16 @@
 $(document).ready(function() {
+  const amenities = {};
   $('input:checkbox').change(function() {
+    const amenity_id = $(this).attr('data-id');
+    const amenity_name = $(this).attr('data-name');
     if ($(this).is(':checked')) {
       //store the Amenity ID in a variable
-      const amenities = [];
-      let amenity_id = $(this).attr('data-name');
-      amenities.push(amenity_id);
-      $('.amenities h4').append(...amenities);
+      amenities[amenity_id] = amenity_name;
     } else {
       //remove the Amenity ID from the variable
-      const index = amenities.indexOf(amenity_id);
-      amenities.slice(index, 1);
+      delete amenities[amenity_id];
     }
+    const amenity_values = Object.values(amenities);
+    $('.amenities h4').text(amenity_values.join(', '));
   });
 });
