e9469aaa3a4566ddc91208093862052665590be5
altoids <noelreeds01@gmail.com>
2025-05-17 12:25:30 +0300

    filter places by amenities

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -10,8 +10,59 @@ $(document).ready(function () {
       // remove the Amenity ID from the variable
       delete amenities[amenity_id];
     }
-    const amenity_values = Object.values(amenities);
-    $('.amenities h4').text(amenity_values.join(', '));
+    const selected_amenities = Object.values(amenities);
+    $('.amenities h4').text(selected_amenities.join(', '));
+    // filter places with amenities selected
+    $('button').click(function() {
+    $.ajax({
+        url: 'http://172.21.68.106:5001/api/v1/places_search/',
+        type: 'POST',
+        contentType: 'application/json',
+        dataType: 'json',
+        data: '{}',
+        success: function(places, textStatus) {
+          // check if selected amenities are a subset of place's amenities
+          // and populate places with selected amenities
+          // but first, retrieve amenities tied to each place and compare
+          for (const place of places) {
+            $.ajax({
+              url : `http://172.21.68.106:5001/api/v1/places/${place.id}/amenities`,
+              type: 'GET',
+              contentType: 'application/json',
+              dataType: 'json',
+              success: function(places_amenities, textStatus) {
+                if (selected_amenities.every(amenity => places_amenities.includes(amenity))) {
+                  //append place with selected amenities
+            	  const article = $('<article></article>').appendTo($('section.places'));
+          		  const title = $('<div></div>').addClass('title_box').appendTo(article);
+                  $(`<h2>${place.name}</h2>`).appendTo(title);
+                  $(`<div>${place.price_by_night}</div>`).addClass('price_by_night').appendTo(title);
+                  // create an info. div element
+                  const info = $('<div></div>').addClass('information').appendTo(article);
+                  const guests = $(`<div>${place.max_guest} Guest${place.max_guest > 1 ? 's': ''}</div>`)
+                    .addClass('max_guest')
+            		.appendTo(info);
+          	      const rooms = $(`<div>${place.number_rooms} Bedroom${place.number_rooms > 1 ? 's': ''}</div>`)
+            		.addClass('number_rooms')
+            		.appendTo(info);
+          		  const bathrooms = $(`<div>${place.number_bathrooms} Bathroom${place.number_bathrooms > 1 ? 's' : ''}</div>`)
+            		.addClass('number_bathrooms')
+            		.appendTo(info);
+          		  // create a user
+          	  	  const uri = `http://172.21.68.106:5001/api/v1/users/${place.user_id}`;
+          		  $.get(uri, function(users) {
+            		const user = $(`<div><b>Owner: </b>${users.first_name} ${users.last_name}</div>`)
+              	      .addClass('user')
+              		  .appendTo(article);
+            		const desc = $(`<div>${place.description}<div>`).addClass('description').appendTo(article);
+                  });
+                }
+            }
+          });
+          }
+        }
+        });
+    });
   });
 });
 
@@ -20,7 +71,6 @@ $(document).ready(function () {
     url: 'http://127.0.0.1:5001/api/v1/status/',
     dataType: 'json',
     success: function (res) {
-      console.log(res.status);
       if (res.status === 'OK') {
         $('div#api_status').addClass('available');
       } else {
